a73f216b80f0df428c442d63699fcad19fb599a2
Tamara Alberti <tamara.alberti@abas.de>
2020-03-27 14:33:47 +0100

    include attributes of super class

--- src/main/java/vehicles/Car.java
+++ src/main/java/vehicles/Car.java
@@ -7,13 +7,15 @@ public class Car extends Vehicle {
 	private String brand;
 	private double height;
 
-	public Car(String brand, double height) {
+	public Car(String brand, double height, String color, String licenseplate) {
+		setColor(color);
+		setLicenseplate(licenseplate);
 		this.brand = brand;
 		this.height = height;
 	}
 
 	@Override
 	public String toString() {
-		return "Car -> " + "brand: '" + brand + "', height: '" + height + "'";
+		return "Car -> " + "brand: '" + brand + "', height: '" + height + "', " + super.toString();
 	}
 }
