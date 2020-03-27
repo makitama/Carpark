1b1d92f0885be29003af2a2330581921f8cdcc48
Tamara Alberti <tamara.alberti@abas.de>
2020-03-27 14:29:26 +0100

    include logik of CommandoList

--- src/main/java/commandos/CommandoList.java
+++ src/main/java/commandos/CommandoList.java
@@ -9,19 +9,32 @@ import main.java.services.ParkingSpotService;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommandList {
+public class CommandoList {
 
-	Map <String, Commando> commandoMap;
+	private Map<String, Commando> commandoMap;
+	private final Carpark carpark;
+	private final CarparkService carparkService;
+	private final ParkingService parkingService;
+	private final ParkingSpotService parkingSpotService;
 
-	public CommandList(Map<String, Commando> commandoMap) {
-		this.commandoMap = commandoMap;
+	public CommandoList(Carpark carpark,  CarparkService carparkService, ParkingService parkingService, ParkingSpotService parkingSpotService) {
+		this.carpark = carpark;
+		this.carparkService = carparkService;
+		this.parkingService = parkingService;
+		this.parkingSpotService = parkingSpotService;
+		getCommandos();
 	}
 
-	private void getCommandos(){
-		commandoMap.put("DRIVE_IN", new DriveInCommando());
+	private void getCommandos() {
+		commandoMap = new HashMap<>();
+		commandoMap.put("DRIVE_IN", new DriveInCommando(carparkService));
 		commandoMap.put("DRIVE_OUT", new DriveOutCommando());
 		commandoMap.put("ENDE", new EndeCommando());
 		commandoMap.put("PARKINGSPOTLEFT", new ParkingSpotLeftCommando());
 		commandoMap.put("HELP", new HelpCommando());
+	}
+
+	public Map<String, Commando> getCommandoMap(){
+		return commandoMap;
 	}
 }
