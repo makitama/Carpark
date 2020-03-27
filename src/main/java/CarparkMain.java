0a515e3c337adcc2356d4da9c772a2a4044bf3f2
Tamara Alberti <tamara.alberti@abas.de>
2020-03-27 14:32:41 +0100

    optimize methods, include logic

--- src/main/java/CarparkMain.java
+++ src/main/java/CarparkMain.java
@@ -9,11 +9,49 @@ import main.java.services.ParkingService;
 import main.java.services.ParkingSpotService;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
 
 public class CarparkMain {
-	public static void main(String[] args) throws IOException {
+
+	private static final String nextCommand = "Und dann?";
+
+	public static void printWelcomeMessage(){
+		//TODO hier oder in eigener klasse? Darf die main methoden und attribute enthalten
+		System.out.println("Herzlich Willkommen im Parkhaus!");
+		System.out.println("Bitte geben Sie ihren Befehl im Format <Befehlsname> <Param>=<Paramwert> ein:");
+		System.out.println("Möchten Sie die Application beenden, geben Sie bitte \"ende\" ein");
+		System.out.println("Geben Sie \"help\" ein, um eine Liste der einzelnen Commandos zu erhalten");
+	}
+
+	public static CommandoList initializeCommandoList() throws IOException {
 		ReadConfigFile properties = new ReadConfigFile();
 		Carpark carpark = new Carpark(properties.getPropertyValues());
+		CarparkService carparkService = new CarparkService(carpark);
+		ParkingService parkingService = new ParkingService(carpark, carparkService);
+		ParkingSpotService parkingSpotService = new ParkingSpotService(carpark);
+		return new CommandoList(carpark, carparkService, parkingService, parkingSpotService);
+	}
 
+	public static void main(String[] args) throws IOException {
+		printWelcomeMessage();
+		CommandoList commandoList = initializeCommandoList();
+		Map<String, Commando> commandos = commandoList.getCommandoMap();
+		CommandoService commandoService = new CommandoService();
+		Scanner scanner = new Scanner(System.in);
+
+		do{
+			try{
+				//Todo: validate();
+				commandoService.commandoParser(scanner.nextLine());
+				commandoService.checkIfCommandoExists(commandos);
+				String commando = commandoService.getCommando();
+				commandos.get(commando).execute(commandoService.getParameters());
+				System.out.println(nextCommand);
+			} catch(Exception ex){
+				System.out.println(ex.getMessage());
+				System.out.println(nextCommand);
+			}
+		} while(true);
 	}
 }
