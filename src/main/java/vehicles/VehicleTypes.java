e21ceb5db0a8eb9ed2e2715f79da43deaedc5e36
Tamara Alberti <tamara.alberti@abas.de>
2020-03-27 14:35:34 +0100

    include method to create a new Vehicle considering the type of the vehicle

--- src/main/java/vehicles/VehicleTypes.java
+++ src/main/java/vehicles/VehicleTypes.java
@@ -7,6 +7,22 @@ public enum VehicleTypes {
 	MOTORCYCLE("Motorcycle"),
 	CAR("Car");
 
-	ParkingSpotTypes(String value) {
+	private String value;
+	VehicleTypes(String value) {
+	}
+
+	public Vehicle createNewVehicle(List<String> parameters){
+		Vehicle vehicle;
+		switch(value){
+			case "Car" :
+				vehicle = new Car(parameters);
+				break;
+			case "Motorcycle" :
+				vehicle = new Motorcycle(parameters);
+				break;
+			default:
+				throw new IllegalStateException("Unexpected value: " + value);
+		}
+		return vehicle;
 	}
 }
