f8ca363dd421a8b19e4788f50dc6fda63c494e99
Tamara Alberti <tamara.alberti@abas.de>
2020-03-27 14:37:32 +0100

    add class CommandNotFoundException

---
+++ src/main/java/exceptions/CommandNotFoundException.java
@@ -0,0 +1,9 @@
+package main.java.exceptions;
+
+public class CommandNotFoundException {
+
+	public CommandNotFoundException(String commando) {
+		//todo fill in command not found exception
+	}
+
+}
