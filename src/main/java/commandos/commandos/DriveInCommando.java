9340bf7a4d8be632f1638564b1d97c58a24e550a
Tamara Alberti <tamara.alberti@abas.de>
2020-03-30 14:10:51 +0200

    change to use DriveInCommandoParamsFactory.

--- src/main/java/commandos/commandos/DriveInCommando.java
+++ src/main/java/commandos/commandos/DriveInCommando.java
@@ -1,16 +1,31 @@
 package main.java.commandos.commandos;
 
-import main.java.vehicles.VehicleTypes;
 import main.java.commandos.Commando;
+import main.java.commandos.commandos.Factories.DriveInCommandoParamsFactory;
 import main.java.services.CarparkService;
 import main.java.vehicles.Vehicle;
+import main.java.vehicles.VehicleTypes;
 
 import java.util.List;
 
 public class DriveInCommando implements Commando {
+
+	private CarparkService carparkService;
+
+	public DriveInCommando(CarparkService carparkService) {
+		this.carparkService = carparkService;
+	}
+
 	@Override
 	public void execute(List<String> parameters) {
-
+		DriveInCommandoParamsFactory driveInCommandoParamsFactory = new DriveInCommandoParamsFactory(parameters);
+		for (VehicleTypes possibleVehicleType : VehicleTypes.values()) {
+			if (possibleVehicleType.getValue().equalsIgnoreCase(driveInCommandoParamsFactory.getType())) {
+				Vehicle vehicle = possibleVehicleType.createNewVehicle(driveInCommandoParamsFactory);
+				carparkService.driveIn(vehicle);
+			}
+		}
+		//todo exception fahrzeug kann net im parkhaus parken
 	}
 
 	@Override
@@ -22,6 +37,4 @@ public class DriveInCommando implements Commando {
 	public String getParameterList() {
 		return "";
 	}
-
-
 }
